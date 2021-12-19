90a091c075d3dae50355b90e0e352fc122d1d778
Lenny3D <iamaveryhotgrill@gmail.com>
2021-12-19 17:50:20 +0100

    Prepare for public release

--- import_scvi_materials.py
+++ import_scvi_materials.py
@@ -1,4 +1,23 @@
 #!/usr/bin/env python3
+
+# Copyright (c) 2021 Lenny3D
+#
+# This software is provided 'as-is', without any express or implied
+# warranty. In no event will the authors be held liable for any damages
+# arising from the use of this software.
+
+# Permission is granted to anyone to use this software for any purpose,
+# including commercial applications, and to alter it and redistribute it
+# freely, subject to the following restrictions:
+# 
+# 1. The origin of this software must not be misrepresented; you must not
+#    claim that you wrote the original software. If you use this software
+#    in a product, an acknowledgment in the product documentation would be
+#    appreciated but is not required.
+# 2. Altered source versions must be plainly marked as such, and must not be
+#    misrepresented as being the original software.
+# 3. This notice may not be removed or altered from any source distribution.
+
 from enum import Enum
 from collections import namedtuple
 from pathlib import Path
@@ -437,8 +456,8 @@ def get_eye_highlight_node(r: ResourceResolver, forceCreate: bool = False) -> bp
     
     return node_tree
 
-def setup_materials():
-    r: ResourceResolver = ResourceResolver()
+def setup_materials(r: ResourceResolver):
+    
     
     eye_data = ""
     
@@ -662,4 +681,5 @@ def setup_materials():
                     base_color_node.image = bpy.data.images.load(bytes(param_mask_texture_path), check_existing=True)
                     
 if __name__ == "__main__":
+    r: ResourceResolver = ResourceResolver()
     setup_materials()
